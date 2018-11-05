749617a59a731cd6c565cca336aed0295b88cb25
Urinbasarov Dias <jentle.men29@gmail.com>
2018-11-05 15:52:32 +0600

    done

---
+++ XmlParser/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace XmlParser
+{
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public DateTime PubDate { get; set; }
+        public void Save()
+        {
+            using (StreamWriter writer = new StreamWriter("file.txt", true))
+            {
+                writer.WriteLine(Title);
+                writer.WriteLine(Link);
+                writer.WriteLine(Description);
+                writer.WriteLine(PubDate);
+            }
+        }
+    }
+}
